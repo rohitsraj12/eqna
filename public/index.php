4e4f90f4c31c16e6c93004a60617e50274739c0d
rohitsraj12 <rohitsraj12@gmail.com>
2020-04-07 16:59:03 +0530

    Update index.php

--- public/index.php
+++ public/index.php
@@ -15,14 +15,217 @@
       </div>
       <!-- end body banner -->
       <div class="body__container">
-            <div class="home__page">
+            <header class="page__home">
                   <main>
-                        
+                        <section class="section__service">
+                              <div class="section__heading">
+                                    <header class="heading-primary align-center">
+                                          <span class="heading-primary--sub">special</span>
+                                          <span class="heading-primary--main">services</span>
+                                    </header>
+                              </div>
+                              <header class="section__body flex wrap__content">
+                                    <article class="flex__block-3 section__service-block">
+                                          <div class="service__image"></div>
+                                          <header class="service__header">
+                                                <h2 class="heading-secondary">
+                                                cutting service
+                                                </h2>
+                                          </header>
+                                          <div class="service__body">
+                                                <p>
+                                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum ut ad velit! Vero, animi mollitia?
+                                                </p>
+                                          </div>
+                                    </article>
+                                    <article class="flex__block-3 section__service-block">
+                                          <div class="service__image"></div>
+                                          <header class="service__header">
+                                                <h2 class="heading-secondary">
+                                                      beauty service
+                                                </h2>
+                                          </header>
+                                          <div class="service__body">
+                                                <p>
+                                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem necessitatibus explicabo maxime cupiditate fuga, veniam assumenda doloribus ut nihil incidunt.
+                                                </p>
+                                          </div>
+                                    </article>
+                                    <article class="flex__block-3 section__service-block">
+                                          <div class="service__image"></div>
+                                          <header class="service__header">
+                                                <h2 class="heading-secondary">
+                                                      coloring service
+                                                </h2>
+                                          </header>
+                                          <div class="service__body">
+                                                <p>
+                                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero rerum sed facere id perspiciatis quia quam!
+                                                </p>
+                                          </div>
+                                    </article>
+                                    <article class="flex__block-3 section__service-block">
+                                          <div class="service__image"></div>
+                                          <header class="service__header">
+                                                <h2 class="heading-secondary">
+                                                      make up service
+                                                </h2>
+                                          </header>
+                                          <div class="service__body">
+                                                <p>
+                                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt quidem nam minima fuga dolorem a voluptatibus impedit.
+                                                </p>
+                                          </div>
+                                    </article>
+                                    <article class="flex__block-3 section__service-block">
+                                          <div class="service__image"></div>
+                                          <header class="service__header">
+                                                <h2 class="heading-secondary">
+                                                package deals
+                                                </h2>
+                                          </header>
+                                          <div class="service__body">
+                                                <p>
+                                                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium omnis nisi quidem rerum natus .
+                                                </p>
+                                          </div>
+                                    </article>
+                                    <article class="flex__block-3 section__service-block">
+                                          <div class="service__image"></div>
+                                          <header class="service__header">
+                                                <h2 class="heading-secondary">
+                                                other services
+                                                </h2>
+                                          </header>
+                                          <div class="service__body">
+                                                <p>
+                                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum ea aspernatur repellendus ex sed voluptates, minima accusamus eos.
+                                                </p>
+                                          </div>
+                                    </article>
+                              </div>
+                        </section>
+                        <!-- end section service -->
+
+                        <section class="section__certification">
+                              <div class="section__heading">
+                                    <header class="heading-primary align-center">
+                                          <span class="heading-primary--sub">beauty</span>
+                                          <span class="heading-primary--main">certification</span>
+                                    </header>
+                              </div>
+                              <div class="section__body flex wrap__content">
+                                    <div class="flex__block-2">
+                                          <header class="certification__header">
+                                                <h2 class="heading-tertiary">
+                                                ABOUT CERTIFICATION PROGRAMS 
+                                                </h2>
+                                                <sub class="sub-heading-primary">
+                                                WELCOME TO THE IQNA BEAUTY SALON
+                                                </sub>
+                                          </header>
+                                          <div class="certificagtion__body">
+                                                <p>
+                                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis distinctio tempora, officiis molestiae minima numquam doloribus ducimus reiciendis. Voluptatibus harum tempora, esse nostrum assumenda odit suscipit labore enim distinctio vel blanditiis doloribus minima. Recusandae.
+                                                </p>
+                                          </div>
+                                    </div>
+                                    <div class="flex__block-2">
+                                          <div class="flex">
+                                          <article class="flex__block-2 section__certification-block">
+                                                <header class="certification__header">
+                                                      <h2 class="heading-secondary">
+                                                            coloring certification
+                                                      </h2>
+                                                </header>
+                                                <div class="certification__body">
+                                                      <p>
+                                                      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero rerum sed facere id perspiciatis quia quam!
+                                                      </p>
+                                                </div>
+                                          </article>
+                                          <article class="flex__block-2 section__certification-block">
+                                                <div class="certification__image"></div>
+                                                <header class="certification__header">
+                                                      <h2 class="heading-secondary">
+                                                            make up certification
+                                                      </h2>
+                                                </header>
+                                                <div class="certification__body">
+                                                      <p>
+                                                      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt quidem nam minima fuga dolorem a voluptatibus impedit.
+                                                      </p>
+                                                </div>
+                                          </article>
+                                          <article class="flex__block-2 section__certification-block">
+                                                <div class="certification__image"></div>
+                                                <header class="certification__header">
+                                                      <h2 class="heading-secondary">
+                                                      package deals
+                                                      </h2>
+                                                </header>
+                                                <div class="certification__body">
+                                                      <p>
+                                                      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium omnis nisi quidem rerum natus .
+                                                      </p>
+                                                </div>
+                                          </article>
+                                          <article class="flex__block-2 section__certification-block">
+                                                <div class="certification__image"></div>
+                                                <header class="certification__header">
+                                                      <h2 class="heading-secondary">
+                                                      other certifications
+                                                      </h2>
+                                                </header>
+                                                <div class="certification__body">
+                                                      <p>
+                                                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum ea aspernatur repellendus ex sed voluptates, minima accusamus eos.
+                                                      </p>
+                                                </div>
+                                          </article>
+                                          </div>
+                                    </div>
+                              </div>
+                        </section>
+                        <!-- end section certification -->
                   </main>
             </div>
             <!-- end home page -->
       </div>
       <!-- end body container -->
+
+      
+        
+    <!-- <div class="grid-text">
+      <div class="row">
+        <div class="col-1-of-2">1 of 2</div>
+        <div class="col-1-of-2">1 of 2</div>
+      </div>
+      <div class="row">
+        <div class="col-1-of-3">1 of 3</div>
+        <div class="col-1-of-3">1 of 3</div>
+        <div class="col-1-of-3">1 of 3</div>
+      </div>
+      <div class="row">
+        <div class="col-1-of-3">1 of 3</div>
+        <div class="col-2-of-3">2 of 3</div>
+      </div>
+      <div class="row">
+        <div class="col-1-of-4">1 of 4</div>
+        <div class="col-1-of-4">1 of 4</div>
+        <div class="col-1-of-4">1 of 4</div>
+        <div class="col-1-of-4">1 of 4</div>
+      </div>
+      <div class="row">
+        <div class="col-1-of-4">1 of 4</div>
+        <div class="col-1-of-4">1 of 4</div>
+        <div class="col-2-of-4">2 of 4</div>
+      </div>
+      <div class="row">
+        <div class="col-1-of-4">1 of 4</div>
+        <div class="col-3-of-4">3 of 4</div>
+      </div>
+    </div> -->
 
 <?php 
       include("../private/required/public/footer.php");
