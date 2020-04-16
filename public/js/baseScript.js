c476b76d80a42658d341cc5290b853f1aa54a1e9
rohitsraj12 <rohitsraj12@gmail.com>
2020-04-16 17:59:01 +0530

    Update baseScript.js

--- public/js/baseScript.js
+++ public/js/baseScript.js
@@ -1,5 +1,24 @@
-$(document).ready(function() {
-  $("#header__toggle").click(function() {
-    $(".header__link").toggle();
+$(document).ready(function () {
+  // mobile navigation
+  $("svg").click(function () {
+    $(".header__link").slideToggle(2000);
+  });
+
+  // scroll functions
+  $(window).scroll(function () {
+    var winTop = $(document).scrollTop();
+    console.log(winTop);
+
+    if (winTop > 300) {
+      $("#section__header>header").removeClass("off-scroll__header");
+      $("#section__header>header").removeClass("wrap__section");
+      $("#section__header").addClass("on-scroll__body-header");
+      $("#section__header>header").addClass("on-scroll__header");
+    } else if (winTop < 50) {
+      $("#section__header").removeClass("on-scroll__body-header");
+      $("#section__header>header").removeClass("on-scroll__header");
+      $("#section__header>header").addClass("off-scroll__header");
+      $("#section__header>header").addClass("wrap__section");
+    }
   });
 });
