4a5b27754402cbc3ef0696a36e85e677d1925927
rohitsraj12 <rohitsraj12@gmail.com>
2020-04-16 17:58:35 +0530

    Update footer.php

--- private/required/public/footer.php
+++ private/required/public/footer.php
@@ -1,7 +1,36 @@
 
             <div class="body__footer">
-              <footer>
-
+              <footer class="wrap__content">
+                  <div class="row">
+                    <div class="col-1-of-4">
+                      <header class="heading-secondary">
+                        quick link
+                      </header>
+                    </div>
+                    <div class="col-1-of-4">
+                      <header class="heading-secondary">
+                        quick link
+                      </header>
+                    </div>
+                    <div class="col-1-of-4">
+                      <header class="heading-secondary">
+                        quick link
+                      </header>
+                    </div>
+                    <div class="col-1-of-4">
+                      <header class="heading-secondary">
+                        quick link
+                      </header>
+                    </div>
+                  </div>
+                  <div class="wrap__copy--right flex">
+                    <div class="flex-2-block">
+                      hi
+                    </div>
+                    <div class="flex-2-block">
+                      there
+                    </div>
+                  </div>
               </footer>
             </div>
             <!-- end body footer -->
@@ -9,26 +38,5 @@
       <!-- script -->
       <script src="<?=base_url()?>js/jQuery.v3.4.1.js"></script>
       <script src="<?=base_url()?>js/baseScript.js"></script>
-    <script>
-      $(document).ready(function() {
-        $(window).scroll(function() {
-          var winTop = $(document).scrollTop();
-          console.log(winTop);
-
-          if (winTop > 130 && winTop < 160) {
-            $("#section__header").addClass("animated fadeInDown show");
-          }
-          if (winTop > 200 && winTop < 550) {
-            $("#left__product").addClass("animated fadeInLeft show");
-            $("#right__product").addClass("animated fadeInRight show");
-          }
-
-          if (winTop > 900 && winTop < 1030) {
-            $("#left__about").addClass("animated fadeInLeft show");
-            $("#right__about").addClass("animated fadeInRight show");
-          }
-        });
-      });
-    </script>
 </body>
 </html>
